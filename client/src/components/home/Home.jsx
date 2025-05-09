23ee426510601d969f4d6de5c304669fa0e622f6
dmtfvn <dmtfvn@gmail.com>
2025-05-09 16:33:28 +0300

    Fix Home page layout

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,8 +1,15 @@
 import SimpleCard from '../cards/SimpleCard.jsx';
 
+import { useLatest } from '../../api/crudApi.js';
+
+import Spinner from '../spinner/Spinner.jsx';
+import EmptySpaceMsg from '../empty-space-msg/EmptySpaceMsg.jsx';
+
 export default function Home() {
+  const { latest, loading } = useLatest();
+
   return (
-    <section className="flex-center flex-col max-w-full">
+    <section className="flex flex-col items-center max-w-[43em] w-full">
       <div className="flex-center flex-col max-w-[40em] w-full mt-20 px-5">
         <p className="text-4xl text-gradient-l text-center font-light">
           Eager to show your art, but wonder where to put it?
@@ -17,20 +24,37 @@ export default function Home() {
         <p className="text-[2em] text-[var(--color-gy-ish)] my-5">&#x2193;</p>
       </div>
 
-      <div className="max-w-[55em] w-full py-10 divide-y divide-black">
+      <div className="w-full py-10 divide-y divide-black">
         <section className="flex justify-center py-10">
-          <div className="flex-center max-w-[14em] w-full min-h-[19em] shadow-card-slot rounded-2xl px-4">
-            <SimpleCard image="https://idsb.tmgrup.com.tr/ly/uploads/images/2020/10/13/64827.jpg" />
+          <div className="relative flex-center max-w-[14em] w-full min-h-[19em] shadow-card-slot rounded-2xl px-4">
+            {/* <SimpleCard image="https://idsb.tmgrup.com.tr/ly/uploads/images/2020/10/13/64827.jpg" /> */}
+            <EmptySpaceMsg
+              message="For the one with most likes"
+            />
           </div>
         </section>
 
         <section className="flex justify-center py-10">
-          <div className="flex items-center justify-evenly flex-wrap max-w-[44.5em] w-full min-h-[19em] shadow-card-slot rounded-2xl p-4 gap-13">
-            <SimpleCard image="https://wallpaperaccess.com/full/1490234.jpg" />
+          <div className="relative flex items-center justify-evenly flex-wrap w-full min-h-[19em] shadow-card-slot rounded-2xl p-4 gap-10">
+            {loading &&
+              <Spinner />
+            }
 
-            <SimpleCard image="https://palmettowebdesign.com/wp-content/uploads/2015/08/White-Space.jpg" />
+            {latest.map(a => (
+              <SimpleCard
+                key={a._id}
+                {...a}
+              />
+            ))}
 
-            <SimpleCard image="https://i.pinimg.com/736x/c1/2c/a7/c12ca701ca2fc38111f374336af00b7d.jpg" />
+            {!loading && !latest.length
+              ?
+              <EmptySpaceMsg
+                message="Latest three additions will show up here"
+              />
+              :
+              null
+            }
           </div>
         </section>
       </div>
