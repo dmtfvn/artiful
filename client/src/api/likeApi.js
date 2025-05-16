1b73c2c1dc48939dc65cfbb02e6152c7d8e7485e
dmtfvn <dmtfvn@gmail.com>
2025-05-16 20:58:05 +0300

    Add like api

---
+++ client/src/api/likeApi.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+
+import useUserContext from '../hooks/useUserContext.js';
+import request from '../utils/request.js';
+
+const url = 'http://localhost:3030/data/likes';
+
+export const useArtIdLike = (_id, artId) => {
+  const [artLike, setArtLike] = useState([]);
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams({
+      where: `_ownerId="${_id}" AND artId="${artId}"`,
+    });
+
+    request.get(`${url}?${searchParams.toString()}`)
+      .then(data => {
+        setArtLike(data);
+      })
+
+  }, [_id, artId]);
+
+  return {
+    artLike,
+  };
+}
+
+export const useAddLike = () => {
+  const { accessToken } = useUserContext();
+
+  const like = (artId) => {
+    return request.post(url, artId, accessToken);
+  }
+
+  return {
+    like,
+  };
+}
+
+export const useRemoveLike = () => {
+  const { accessToken } = useUserContext();
+
+  const unlike = (likeId) => {
+    return request.delete(`${url}/${likeId}`, undefined, accessToken);
+  }
+
+  return {
+    unlike,
+  };
+}
